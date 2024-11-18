b6a218161d3c366b2608b24176952faf0b5207a7
Ysgrmr13 <Ysgrmr13@gmail.com>
2024-11-18 17:55:34 +0300

    CreateQuest

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
-public class Util {
-    // реализуйте настройку соеденения с БД
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class  Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/dbtable";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
